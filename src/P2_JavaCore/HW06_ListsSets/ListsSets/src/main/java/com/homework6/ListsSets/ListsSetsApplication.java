834b06d75816cca807e2aa7efc184bf5a3475d16
NuzGooL <ardeniaanderland@gmail.com>
2023-09-18 18:28:11 +0300

    Initial commit.  Spring Boot + Web

---
+++ src/P2_JavaCore/HW06_ListsSets/ListsSets/src/main/java/com/homework6/ListsSets/ListsSetsApplication.java
@@ -0,0 +1,13 @@
+package com.homework6.ListsSets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListsSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListsSetsApplication.class, args);
+	}
+
+}
---
+++ src/P2_JavaCore/HW06_ListsSets/ListsSets/src/test/java/com/homework6/ListsSets/ListsSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework6.ListsSets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsSetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
